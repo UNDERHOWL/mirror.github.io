a48d74b03ba4624b4e18e7ac5c3074ef1bc81c04
masui <mahth.png@gmail.com>
2021-11-30 11:07:37 +0900

    コードを整理しました

--- public/js/compare_url.js
+++ public/js/compare_url.js
@@ -1,24 +1,19 @@
-var new_url = "jkeojkwal"
+var new_url = "nmkvdslzv"
 var old_url
 
-function compare(){
+$(document).ready(function(){
     old_url = $.cookie("old_url");
     console.log('新しいURLは' + new_url)
     console.log('古いURLは' + old_url)
     if (new_url === old_url || new_url === null) {
-        console.log('同じURLです');
+        console.log('同じURLのため通知は表示しません');
         $(".notification_sentence").removeClass("notification_sentence_block");
     } else if(new_url != old_url) {
-        console.log('違うURLです');
+        console.log('違うURLのため通知を表示し、old_urlを上書きします。');
         $(".notification_sentence").addClass("notification_sentence_block");
         old_url = new_url;
         $.cookie("old_url",old_url);
         console.log(old_url)
     }
-}
-
-$(document).ready(function(){
-    compare();
-});
-
+})
 
